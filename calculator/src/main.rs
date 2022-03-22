b7ae52f97999db25d1aaf502ab5b72cc87279252
voidash <ashish.thapa477@gmail.com>
2022-03-22 22:32:36 +0545

    lalrpop calculator

---
+++ calculator/src/main.rs
@@ -0,0 +1,33 @@
+#[macro_use] 
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub calculator1);
+
+
+#[test]
+fn calculator1() {
+    assert!(calculator1::TermParser::new().parse("22").is_ok());
+    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+}
+
+lalrpop_mod!(pub calculator2);
+
+#[test]
+fn calculator2() {
+    assert!(calculator2::ExprParser::new().parse("22*34+12").is_ok());
+    assert_eq!(calculator2::ExprParser::new().parse("22*34+12").unwrap(), 22*34+12);
+}
+
+lalrpop_mod!(pub calculator4);
+pub mod ast;
+
+#[test]
+fn calculator4() {
+    let expr = calculator4::ExprParser::new().parse("22*44+23").unwrap();
+    println!("{:?}",expr);
+}
+
+fn main() {
+    println!("test");
+}
