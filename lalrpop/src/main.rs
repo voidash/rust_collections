96569de9a79e7000b6caa15ccef64d46cf30dd57
voidash <ashish.thapa477@gmail.com>
2022-03-28 21:28:10 +0545

    add primitive 8085 assembly parser

--- lalrpop/src/main.rs
+++ lalrpop/src/main.rs
@@ -1,22 +1,38 @@
+pub mod utils;
+
 #[macro_use]
 extern crate lalrpop_util;
 
-lalrpop_mod!(pub calculator1);
+lalrpop_mod!(pub assembly);
 
 
-#[test]
-fn calculator1() {
-    assert!(calculator1::TermParser::new().parse("22").is_ok());
-}
-
-
-lalrpop_mod!(pub calculator2);
 
 #[test]
-fn calculator2() {
-    assert!(calculator2::TermParser::new().parse("4+3").is_ok());
+fn literal_check() {
+   assert_eq!(assembly::DecParser::new().parse("12"),Ok(12));
 }
+
+#[test]
+fn register_check() {
+    assert_eq!(assembly::RegisterParser::new().parse("a").unwrap(),"a");
+    assert_ne!(assembly::RegisterParser::new().parse("Z"),Ok("Z"));
+}
+
+#[test]
+fn instruction_check() {
+    assert_eq!(assembly::InstructionParser::new().parse("mov").unwrap(),"mov");
+    assert_ne!(assembly::InstructionParser::new().parse("ldaxx"),Ok("ldaxx"));
+}
+
+#[test]
+fn opcode_check() {
+    println!("{:?}",assembly::OpcodeParser::new().parse("check: mov a    , b").unwrap());
+    println!("{:?}",assembly::OpcodeParser::new().parse("mvi a, 32D").unwrap());
+    println!("{:?}",assembly::OpcodeParser::new().parse("mvi a").unwrap());
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    println!("this is assembly");
 }
+
